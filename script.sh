6bec0bfef9a3b354b2dde7debed7cc37564582a5
Burtseva Mariya <mariyaburceva9898@mail.ru>
2023-02-15 22:00:19 +0300

    add script.sh

---
+++ script.sh
@@ -0,0 +1,16 @@
+cd folder_3
+mkdir folder_1_1
+mkdir folder_2_2
+mkdir folder_3_3
+cd folder_1_1
+touch 1_1.txt
+touch 2_2.txt
+touch 3_3.txt
+touch 1_1.json
+touch 2_2.json
+mkdir 1
+mkdir 2
+mkdir 3
+ls
+mv 1_1.txt 1
+mv 1_1.json 1
